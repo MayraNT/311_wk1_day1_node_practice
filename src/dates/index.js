ecb8521300b52e00ab41a6ee88d5305bff62fd3e
mayrant <mayrant2004@yahoo.com>
2021-07-05 18:29:56 -0500

    all dates tests passing

--- src/dates/index.js
+++ src/dates/index.js
@@ -1,18 +1,19 @@
 const moment = require('moment');
 
 const today = () => {
-  // write code for dates.today
+  // Return the name of current day. Ex. Monday, Tuesday, etc.
   return moment().format('dddd');
 }
 
 const calendar = () => {
-  // write code for dates.calendar
+  // Return the current date in the format: May 29, 2019
+  // formatted test to reflect July 5, 2021
   return moment().format('MMMM D, YYYY');
 }
 
 const currentTime = () => {
-  // write code for dates.currentTime
-
+  // Return the current time in the format: 03:21:51 PM
+  return moment().format('hh:mm:ss A');
 }
 
 module.exports = {
