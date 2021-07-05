58226b226ae79b455c5c0b02f15724a9272fe963
mayrant <mayrant2004@yahoo.com>
2021-07-05 18:22:32 -0500

    two numbers functions and two dates functions passing, formatted dates test to reflect today's date

--- src/numbers/index.js
+++ src/numbers/index.js
@@ -1,16 +1,22 @@
 const isEven = (num) => {
   // write code for numbers.isEven
-
+  if (num % 2 === 0) {
+    return true
+  }
 }
 
 const sum = (arr) => {
   // write code for numbers.sum
-
+  const reduceFn = (x, y) => x + y;
+  return arr.reduce(reduceFn);
 }
 
 const comboSum = (arr, sum) => {
   // write code for numbers.comboSum
-
+  // if any two numbers in the array equal the sum
+  // if they add up to 14 return true
+  // if they don't, return false
+  
 }
 
 module.exports = {
