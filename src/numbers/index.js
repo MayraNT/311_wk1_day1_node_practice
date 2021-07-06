12691c091e75c1015f67911b781867d665eafc3b
mayrant <mayrant2004@yahoo.com>
2021-07-06 15:47:01 -0500

    numbers tests passing, all tests now passing

--- src/numbers/index.js
+++ src/numbers/index.js
@@ -1,22 +1,32 @@
 const isEven = (num) => {
-  // write code for numbers.isEven
+  // Return a boolean that represents whether a given number is even
   if (num % 2 === 0) {
-    return true
+    return true;
+  } else {
+    return false;
   }
 }
 
 const sum = (arr) => {
-  // write code for numbers.sum
+  // Return a value that represents the sum of an array of numbers
   const reduceFn = (x, y) => x + y;
   return arr.reduce(reduceFn);
 }
 
 const comboSum = (arr, sum) => {
-  // write code for numbers.comboSum
-  // if any two numbers in the array equal the sum
-  // if they add up to 14 return true
+  // Determine if any combination of numbers in an array equals a given sum. Return a boolean
+  // Hint: Loop in loop OR indexOf for diff
+  // if any two numbers in the array add up to sum, return true
   // if they don't, return false
-  
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      if ((arr[i] + arr[j]) === sum) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  }
 }
 
 module.exports = {
--- test/index.test.js
+++ test/index.test.js
@@ -1,23 +1,23 @@
 const { numbers, strings, dates } = require('../src')
 
-// describe('Numbers module', () => {
+describe('Numbers module', () => {
 
-//   test('Check if a given number is even or odd', () => {
-//     const isEven = numbers.isEven(29384)
-//     expect(isEven).toBe(true)
-//   })
+  test('Check if a given number is even or odd', () => {
+    const isEven = numbers.isEven(29384)
+    expect(isEven).toBe(true)
+  })
 
-//   test('Add multiple numbers', () => {
-//     const sum = numbers.sum([1, 2, 3, 4, 5, 6])
-//     expect(sum).toEqual(21)
-//   })
+  test('Add multiple numbers', () => {
+    const sum = numbers.sum([1, 2, 3, 4, 5, 6])
+    expect(sum).toEqual(21)
+  })
 
-//   test('Determine if any combination equals a given sum', () => {
-//     const exists = numbers.comboSum([1, 3, 6, 2, 9], 14)
-//     expect(exists).toBe(false)
-//   })
+  test('Determine if any combination equals a given sum', () => {
+    const exists = numbers.comboSum([1, 3, 6, 2, 9], 14)
+    expect(exists).toBe(false)
+  })
 
-// })
+})
 
 describe('Strings module', () => {
 
@@ -43,29 +43,29 @@ describe('Strings module', () => {
 
 })
 
-// describe('Dates module', () => {
+describe('Dates module', () => {
 
-//   test('Return today\'s day - ex. Monday', () => {
-//     const weekdays = [
-//       'Monday',
-//       'Tuesday',
-//       'Wednesday',
-//       'Thursday',
-//       'Friday'
-//     ]
+  test('Return today\'s day - ex. Monday', () => {
+    const weekdays = [
+      'Monday',
+      'Tuesday',
+      'Wednesday',
+      'Thursday',
+      'Friday'
+    ]
 
-//     const day = dates.today()
-//     expect(weekdays).toContain(day)
-//   })
+    const day = dates.today()
+    expect(weekdays).toContain(day)
+  })
 
-//   test('Return today\'s date in the format: May 29, 2019', () => {
-//     const cal = dates.calendar()
-//     expect(cal).toMatch(/^[A-z]{4} [0-9]{1}, [0-9]{4}$/)
-//   })
+  test('Return today\'s date in the format: May 29, 2019', () => {
+    const cal = dates.calendar()
+    expect(cal).toMatch(/^[A-z]{4} [0-9]{1}, [0-9]{4}$/)
+  })
 
-//   test('Return the current time in the format: 03:21:51 PM', () => {
-//     const time = dates.currentTime()
-//     expect(time).toMatch(/^[0-9]{2}:[0-9]{2}:[0-9]{2} [A-Z]{2}$/)
-//   })
+  test('Return the current time in the format: 03:21:51 PM', () => {
+    const time = dates.currentTime()
+    expect(time).toMatch(/^[0-9]{2}:[0-9]{2}:[0-9]{2} [A-Z]{2}$/)
+  })
 
-// })
+})
